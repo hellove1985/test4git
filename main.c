a461c34280a001de58f45d3d3b420b37781cb56b
pentest <pentest@hellove.net>
2014-01-16 03:51:54 +0800

    This is a branch

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
+    time_t mytime;
+    printf("Year");
+    printf("Month");
+    printf("Day");
     printf("Version: 0.02\n");
     printf("I am main\n");
     return 0;
